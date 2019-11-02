ff3d8b3bcc3ec47497bc3052399cbcc6fb24fd90
Ttayu <ykisoccer.pg@gmail.com>
2019-11-03 08:02:58 +0900

    Implement to get class and score with max confidence.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-from flask import Flask, json, jsonify, make_response, redirect, request
+from flask import Flask, jsonify, request
 from watson_developer_cloud import VisualRecognitionV3
 
 app = Flask(__name__)
@@ -13,12 +13,12 @@ class IBM:
     visual_recognition = VisualRecognitionV3("2019-11-02", iam_apikey=api_key)
 
     @classmethod
-    def classify_best_cool(cls, images_file):
-        result = cls.classify(images_file)
-        return cls.get_score_of_best_cool(result)
+    def classify_fashion_sense(cls, images_file):
+        result = cls._classify(images_file)
+        return cls._get_max_class(result)
 
     @classmethod
-    def classify(cls, images_file):
+    def _classify(cls, images_file):
         with open(images_file, "rb") as f:
             result = cls.visual_recognition.classify(
                 f, threshold="0.0", classifier_ids="DefaultCustomModel_1445172307"
@@ -26,16 +26,18 @@ class IBM:
         return result
 
     @classmethod
-    def get_score_of_best_cool(cls, result):
+    def _get_max_class(cls, result):
         classifiers = result["images"][0]["classifiers"]
         fashion_classes = classifiers[0]["classes"]
-        score = None
+        max_score = 0
+        fashion_sense = None
         for element in fashion_classes:
-            if element["class"] == "class4":
-                score = element["score"]
-        if score is None:
-            raise ValueError("not included class4")
-        return score
+            if max_score < element["score"]:
+                max_score = element["score"]
+                fashion_sense = element["class"]
+        if fashion_sense is None:
+            raise ValueError("not classified fashon sense.")
+        return fashion_sense, max_score
 
 
 @app.route("/", methods=["POST"])
@@ -47,9 +49,8 @@ def get_image(data):
     image_path = Path(data["image_path"])
     if not Path(image_path).exists():
         raise FileNotFoundError(f"file not found: {image_path}")
-        # return redirect("/error")
-    score = IBM.classify_best_cool(image_path)
-    return {"id": data["id"], "score": score}
+    fashion_sense, score = IBM.classify_fashion_sense(image_path)
+    return {"id": data["id"], "class": fashion_sense, "score": score}
 
 
 @app.route("/test_image", methods=["POST"])
@@ -57,23 +58,16 @@ def post_image():
     data = request.get_json()
     if not data:
         raise ValueError(f"data not found: {data}")
-        # return redirect("/error")
     return jsonify(get_image(data))
 
 
 @app.route("/test_images", methods=["POST"])
 def post_images():
     data = request.get_json()
-    print(data)
     responses = []
     for datum in data:
         responses.append(get_image(datum))
     return jsonify(responses)
-
-
-@app.route("/error")
-def error():
-    return "error"
 
 
 if __name__ == "__main__":
